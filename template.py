f16a326c450005e38786db38dc20d336ce767856
Rajesh201993 <rajesh.venaganti20@gmail.com>
2024-08-21 18:19:04 +0530

    folder structure modified

--- template.py
+++ template.py
@@ -2,26 +2,26 @@ import os
 import logging
 from pathlib import Path
 
-
+#seperate stages ml project like data injection,EDA,feature engineering, model building and model evaluation and compounents are part of pipelens, we have two pipeline first one is training pipeline and infercing pipeline
 
 list_of_files=[
-    ".github/workflows/.getkeep",
-    "src/__init__.py"
-    "src/components/__init.py", #seperate stages ml project like data injection,EDA,feature engineering, model building and model evaluation and compounents are part of pipelens, we have two pipeline first one is training pipeline and infercing pipeline
+    ".github/workflows/.gitkeep", # .gitkeep user for deployment(CICD)
+    "src/__init__.py",
+    "src/components/__init__.py", 
     "src/components/data_injection.py",
     "src/components/data_transformation.py",
     "src/components/model_trainer.py",
     "src/components/model_evaluation.py",
-    "src/pipeline/__init__.py"
+    "src/pipeline/__init__.py",
     "src/pipeline/training_pipeline.py",
     "src/pipeline/prediction_pipeline.py",
     "src/utils/__init__.py",
     "src/utils/utils.py",
     "src/logger/logging.py",
-    "src/exception/__init__.py"
+    "src/exception/__init__.py",
     "src/exception/exception.py",
     "tests/unit/__init__.py",
-    "tests/unit/integration/__init__.py",
+    "tests/integration/__init__.py",
     "init_setup.sh",
     "requirements.txt",
     "requirements_dev.txt",
